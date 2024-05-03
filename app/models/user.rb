f48145959c3e0abe393522607491822219469c42
Daniela Baron <baron.daniela@gmail.com>
2024-05-03 09:30:23 -0400

    allow address to not be entered when creating or editing user

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,13 @@
 #
 class User < ApplicationRecord
   has_one :address, dependent: :destroy
-  accepts_nested_attributes_for :address
+
+  # This version requires the user to always enter an address
+  # accepts_nested_attributes_for :address
+
+  # Give the user the option to leave address blank
+  # although if they enter in even one address field, then address will be associated
+  accepts_nested_attributes_for :address, reject_if: :all_blank
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
