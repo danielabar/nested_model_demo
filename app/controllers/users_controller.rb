89daff63920ed4a5de736e01159393ba6c9d9f41
Daniela Baron <baron.daniela@gmail.com>
2024-05-20 08:03:44 -0400

    add explanatory comments and variations on accepts_nested_attributes_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,6 +67,8 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
+    # Allow address fields to be submitted via nested attributes
+    # params.require(:user).permit(:name, :email)
     params.require(:user).permit(:name, :email, address_attributes: %i[id street city state_province country])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,13 +15,29 @@
 class User < ApplicationRecord
   has_one :address, dependent: :destroy
 
-  # This version requires the user to always enter an address
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  # Require the user to always provide an associated address
   # accepts_nested_attributes_for :address
 
   # Give the user the option to leave address blank
   # although if they enter in even one address field, then address will be associated
   accepts_nested_attributes_for :address, reject_if: :all_blank
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  # Only associate address if user has filled in street, city, and state_province, but disregard country
+  # accepts_nested_attributes_for :address,
+  #                               reject_if: proc { |a|
+  #                                            a[:street].blank? && a[:city].blank? && a[:state_province].blank?
+  #                                          }
+
+  # Same logic as above, but with a method rather than a proc
+  # accepts_nested_attributes_for :address, reject_if: :address_fields_all_blank?
+
+  private
+
+  # Demonstrate use of a method rather than a proc for reject_if option to accepts_nested_attributes_for
+  def address_fields_all_blank?(address_attributes)
+    address_attributes[:street].blank? && address_attributes[:city].blank? && address_attributes[:state_province].blank?
+  end
 end
